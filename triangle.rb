46104fd0582c02f7963042779f330af3af3ba4f9
Yaroslav <yaroslav200124@gmail.com>
2019-05-26 18:35:56 +0300

    End task

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  raise TriangleError, "Length of side <= 0" if (a <= 0 || b <= 0 || c <= 0)
+  if(a == b && b == c)
+	  return :equilateral
+  end
+  raise TriangleError, "Triangle doesn't exist!" if (a + b <= c || a + c <= b || b + c <= a)
+  if(a == b || b == c || a == c)
+  	:isosceles
+  else
+  	:scalene 
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
